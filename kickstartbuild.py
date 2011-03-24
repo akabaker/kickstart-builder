b0acecc9823bde945252b60ef934c66776b787a9
Luke Baker <akabaker@gmail.com>
2011-03-24 11:46:53 -0500

    Renamed to kickstartbuild

---
+++ kickstartbuild.py
@@ -0,0 +1,174 @@
+from string import Template
+from optparse import OptionParser, OptionValueError
+from urlparse import urljoin
+from subprocess import PIPE, Popen
+from sys import argv
+import os
+
+class KickstartBuild(object):
+	"""
+	Class BuildKS: Constructs a kickstart file based on premade template
+
+	"""
+	
+	def __init__(self):
+		"""
+		Builds the parser menu, sets attibutes
+
+		"""
+
+		# Attributes
+		self.template_file = 'ks_template.tmpl'	
+		self.base_url = 'http://rhn.missouri.edu/pub'
+		self.repo_name = 'csgrepo'
+		self.repo_url = '/'.join([self.base_url,self.repo_name])
+		# Array of strings because we have to join these later (I'm lazy)
+		self.valid_releases = ['4', '5', '6']
+		self.valid_archs = ['x86_64', 'i386']
+		self.bootstrap_path = os.path.dirname('/var/www/html/pub/bootstrap/')
+		# args_required should be set to minimum required arguments
+		self.args_required = 5
+
+		# Parser options
+		self.parser = OptionParser(
+			add_help_option=True,
+			usage = "%prog [options] <fqdn> <ipaddress> <netmask> <gateway> <nameserver>",
+			version = "%prog 1.0",
+		)
+
+		self.parser.set_defaults(
+			arch = 'x86_64',
+			release = '5',
+			bootstrap = 'bootstrap.sh',
+		)
+
+		self.parser.add_option('-a', '--arch', action='store', type='string',
+						 help='x86_64 or i386', dest='arch')
+		self.parser.add_option('-r', '--release', action='store',type='string', dest='release',
+						 help='Distribution Release, (i.e., 5 or 6)')
+		self.parser.add_option('-b', '--bootstrap', action='store', type='string', dest='bootstrap',
+						 help='Your bootstrap script (i.e., bakerlu_bootstrap.sh)')
+	
+		# Set options and args
+		(self.options, self.args) = self.parser.parse_args()
+	
+	def check_args(self):
+		"""
+		Validate argument length, arch and release options.
+		Length validation compares number of args vs self.args_required (set in constructor)
+		Arch validation compares the arch option vs self.valid_archs (list, set in constructor)
+		Release validation compares release option vs self.valid_releases (list, set in constructor)
+
+		"""
+
+		if len(self.args) < self.args_required:
+			self.parser.error('Incorrect number of arguments')
+
+		if self.options.arch not in self.valid_archs:
+			raise OptionValueError('Not a valid arch')
+
+		if self.options.release not in self.valid_releases:
+			raise OptionValueError('Not a valid release')
+
+	def validate(self):
+		"""
+		Validate digs the fqdn and IP address. If either of these do not resolve properly, raise an error
+		If the bootstrap option is specified, validate checks to make sure that the script exists
+
+		"""
+
+		# Slice off the last character from the result [:-2]
+		checked_hostname = Popen("dig +short -x %s" % self.args[1], stdout=PIPE, shell=True).communicate()[0][:-2]
+		checked_ip = Popen("dig +short %s" % self.args[0], stdout=PIPE, shell=True).communicate()[0].strip()	
+
+		if checked_hostname != self.args[0] or checked_ip != self.args[1]:
+			raise OptionValueError('Your FQDN and IP do not resolve properly')
+
+		bootstrap = os.path.join(self.bootstrap_path, self.options.bootstrap)
+
+		if os.path.exists(bootstrap) is not True:
+			raise OptionValueError('The specified bootstrap script does not exist')
+
+	def build_url_segments(self):
+		"""
+		Construct a url segment to match the installation directory on rhn
+		ie, rhn.missouri.edu/pub/RHEL5_64
+
+		"""
+		
+		# Build the installation directory
+		install_dir = []
+		install_dir.append('RHEL'+self.options.release)
+
+		if self.options.arch == 'x86_64':
+			install_dir.append('64')
+		else:
+			install_dir.append('i386')
+	
+		# Create the install URL segment
+		install_segment = '_'.join(install_dir)
+
+		# Build the repo URL segment
+		if self.options.arch != 4:
+			repo_segment = '/'.join([self.options.release+'Server', self.options.arch])
+		else:
+			repo_segment = '/'.join([self.options.release, self.options.arch])
+
+		results = {
+			'install_segment': install_segment,
+			'repo_segment': repo_segment,
+		}
+
+		return results
+
+	def build(self, output_dir):
+		"""
+		Constructs template based on the file self.template_file
+
+		"""
+
+		try:
+			f = open(self.template_file, 'r')
+			try:
+				lines = f.read()
+			finally:
+				f.close()
+		except IOError, e:
+			print "Problem reading file: %s" % e
+
+		# Create template from the file
+		tmpl = Template(lines)
+
+		# Call install_dir() to build our url segment
+		url_segments = self.build_url_segments()
+
+		# Substitute template variables with arguments
+		output = tmpl.safe_substitute(
+			# i.e., rhn.missouri.edu/pub/RHEL5_x86
+			install_url = '/'.join([self.base_url, url_segments.get('install_segment')]),
+			hostname = self.args[0],
+			ip = self.args[1],
+			netmask = self.args[2],
+			gateway = self.args[3],
+			dns = self.args[4],
+			bootstrap = self.options.bootstrap,
+			# i.e., rhn.missouri.edu/pub/csgrepo/5Server/x86_64
+			repo_url = '/'.join([self.repo_url, url_segments.get('repo_segment')]),	
+		)
+
+		dir = os.path.dirname(output_dir)
+		# The kickstart filename is <hostname>.ks
+		filename = '.'.join([self.args[0], 'ks'])
+		file = os.path.join(dir, filename)
+
+		if os.path.exists(dir):			
+			try:
+				f = open(file, 'w')
+				try:
+					f.write(output)
+				finally:
+					f.close()
+			except IOError, e:
+				print "Problem reading file: %s" % e
+		else:
+			raise DirectoryError('Directory does not exist')
