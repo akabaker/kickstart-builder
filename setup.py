c9ab89cfe36edcd74a371a88036a3f042fedddf3
Luke Baker <akabaker@gmail.com>
2011-03-29 11:24:30 -0500

    Added IPtables stanza to the csgbase template

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 base_dir = '/home/ljb6tw/kickstart-builder'
 
 setup(
-    name='buildks',
-    version='1.0',
-    py_modules=['kickstartbuild'],
-    scripts=['%s/script/buildks' % base_dir],
-    data_files=[
+    name = 'buildks',
+    version = '1.0',
+    py_modules = ['kickstartbuild'],
+    scripts = ['%s/script/buildks' % base_dir],
+    data_files = [
     	('/etc/buildks', ['%s/conf/buildks.conf' % base_dir]),	
     	('/etc/buildks/templates', [
 			'%s/conf/templates/csgbase.tmpl' % base_dir,
 			'%s/conf/templates/oldbase.tmpl' % base_dir,
     		]),
     	]
-    )
+	)
